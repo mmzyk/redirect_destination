c52b170512b3bd364e39b0cfdf8250ccea06cfbb
Mark Mzyk <mmzyk@programmersparadox.com>
2011-06-02 00:47:30 -0400

    Diagnosing problem with deploy to heroku

--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
-require 'redirect_check'
+require './redirect_check'
 run Sinatra::Application
--- redirect_check.rb
+++ redirect_check.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require ::File.expand_path('../redirect_follower', __FILE__)
 
+get '/' do
+  "To check the final destination if a url redirects, add it as a prameter onto the url for this site. <br />
+  The final destination after all redirects will be reported back, unless the operation reaches the redirect limit (5). <br /><br />
+  For example, to check google.com, copy and past this in the address bar: http://warm-flower-161.heroku.com/google.com <br />
+  Change google.com on the end of the url to check other urls. <br /><br />
+  This is a work in progress and will break for many cases."
+end
+
 get '/:name' do |url|
     if url == 'favicon.ico' 
       #do nothing - not sure why it is trying to grab the favicon
